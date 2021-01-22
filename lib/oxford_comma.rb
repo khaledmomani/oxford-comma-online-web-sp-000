8731fc4b7da87d983e254a169aa39009eb4417cc
Khaled S Momani <khaledmomani4@gmail.com>
2021-01-22 04:35:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ array.join(",")
 elsif array.length == 2 
 array.join(" and ")
 elsif array.length == 3
-return "#{array[1]}, #{array[2]}, and #{array [3]}"
+return "#{array[0]}, #{array[1]}, and #{array [2]}"
 end
 end
