c78772f00fb09d27036762042fddb416790ad4fb
Khaled S Momani <khaledmomani4@gmail.com>
2021-01-22 04:46:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,8 @@ array.join(" and ")
 elsif array.length == 3
 return "#{array[0]}, #{array[1]}, and #{array [2]}"
 elsif array.length > 3
-array.pop.join
+lastOne = array.pop
+array.join(,)
+>>
 end
 end
